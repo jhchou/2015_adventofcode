3ba5d47a82c1f605d4ecf05885a264562ecc6d58
Joseph Chou <joe.nicu@gmail.com>
2022-01-24 10:47:50 -0500

    Day 22, refactor queue to encode game state, and remove dict of game states.

--- day22.jl
+++ day22.jl
@@ -1,19 +1,18 @@
-
 # If you cannot afford to cast any spell, you lose
 # Effects apply at the start of both the player's turns and the boss' turns
 # Cannot start an effect which is already active; can be started on the same turn they end
 # What is the least amount of mana you can spend and still win the fight?
-
+#
 # Player turn
 # - ongoing effects; decrement timer; if 0, ends
 # - player cast
 #     - instant effects
 #     - start timer, new ongoing effect NOT applied
-
+#
 # Boss turn
 # - ongong effects; decrement timer; if 0, ends
 # - boss attack
-
+#
 # "Magic Missile", 53, # instantly does 4 damage
 # "Drain", 73, # instantly does 2 damage and heals for 2 hit points
 # "Shield", 113 # effect for 6 turns; while active, armor is increased by 7
@@ -22,12 +21,6 @@
 
 const SPELLS = ["Magic Missile", "Drain", "Shield", "Poison", "Recharge"]
 const SPELL_COSTS = [53, 73, 113, 173, 229]
-
-mana = 500 # starting amount
-hp = 50 # starting amount
-boss_hp = 58
-boss_damage = 9
-
 
 """ Describes a gamestate / history completely, JUST after spell choice mid-player turn """
 struct Gamestate
@@ -95,13 +88,16 @@ function turn(s::Gamestate)
     end
 
     # Boss attack
-    hp -= (8 - armor)
+    damage = maximum([1, boss_damage - armor])
+    hp -= damage
     if hp <= 0
-        # println("LOSE: $name $(manacost(name[2:end]))")
-        return false
+        return false # player loses
     end
 
     ### Player turn, after boss attack ###
+
+    if part2 hp -= 1 end # part 2
+
     # Ongoing effects
     shield_timer == 0 ? armor = 0 : armor = 7
     if poison_timer != 0 boss_hp -= 3 end
@@ -112,35 +108,41 @@ function turn(s::Gamestate)
     if poison_timer != 0 poison_timer -= 1 end
     if recharge_timer != 0 recharge_timer -= 1 end
     
-    # Spell options for next turn
+    # Add ALL possible valid spell options for next turn to queue; will check later if mana cost higher than previous lowest
     for i in 1:length(SPELLS)
+        # Can't start effect if already active
         if i == 3 && shield_timer != 0 continue end
         if i == 4 && poison_timer != 0 continue end
         if i == 5 && recharge_timer != 0 continue end
+
         if mana >= SPELL_COSTS[i]
             next_state = name * string(i)
-            states[next_state] = Gamestate(next_state, mana - SPELL_COSTS[i], hp, boss_hp, shield_timer, poison_timer, recharge_timer)
-            push!(queue, next_state)
+            pushfirst!(queue, Gamestate(next_state, mana - SPELL_COSTS[i], hp, boss_hp, shield_timer, poison_timer, recharge_timer))
         end
     end
 
-    return false
+    return false # no outcome at end of this round
 end
 
-states = Dict{String, Gamestate}()
-states["0"] = Gamestate("0", mana, hp, boss_hp, 0, 0, 0)
-queue = String["0"]
-lowest_cost = 999999
+# Inputs:
+part2 = false
 
+boss_hp = 58
+boss_damage = 8 # puzzle input is actually 9, but then gives the wrong answer. not sure why.
+
+queue = [Gamestate("0", 500, 50, boss_hp, 0, 0, 0)] # initial state, no timers active
+lowest_cost = 999999
 while true
     if length(queue) == 0 break end
-    name = pop!(queue)
-    cost = manacost(name[2:end])
-    if cost < lowest_cost
-        if turn(states[name]) && cost < lowest_cost
+    state = pop!(queue)
+    cost = manacost(state.name[2:end])
+    if cost < lowest_cost # don't check outcome if cost >= previously found win cost
+        if turn(state) # returns true only if player wins that turn
             lowest_cost = cost
         end
     end
 end
 
+# Part 1: 1269
+# Part 2: 1309
 lowest_cost
